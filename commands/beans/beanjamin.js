77031df33f8f2156d8480f0cbff15cc6fbb852d9
Dan James <41585360+Deehjay@users.noreply.github.com>
2024-10-28 20:31:23 +0000

    temporarily remove bean functionality

--- commands/beans/beanjamin.js
+++ commands/beans/beanjamin.js
@@ -1,44 +1,46 @@
-const { SlashCommandBuilder } = require('discord.js');
-const axios = require('axios');
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
 
 const serpToken = process.env.SERP_API_KEY;
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('beans')
-		.setDescription('Find Ben some beans!'),
-	async execute(interaction) {
-		// Fetch a random image from SerpAPI
-		async function getRandomImage(query) {
-			const serpApiUrl = `https://serpapi.com/search.json?engine=google_images&q=${query}&api_key=${serpToken}`;
-			try {
-				const response = await axios.get(serpApiUrl);
-				const images = response.data.images_results;
+  data: new SlashCommandBuilder()
+    .setName("beans")
+    .setDescription("Find Ben some beans!"),
+  async execute(interaction) {
+    return interaction.reply(
+      "COMMAND IS UNDER MAINTENANCE. DO NOT FEAR, BEN WILL BE ABLE TO ENJOY HIS BEANS AGAIN SOON! :)"
+    );
+    // Fetch a random image from SerpAPI
+    // async function getRandomImage(query) {
+    // 	const serpApiUrl = `https://serpapi.com/search.json?engine=google_images&q=${query}&api_key=${serpToken}`;
+    // 	try {
+    // 		const response = await axios.get(serpApiUrl);
+    // 		const images = response.data.images_results;
 
-				if (images && images.length > 0) {
-					// Pick a random image from the results
-					const randomImage = images[Math.floor(Math.random() * images.length)].original;
-					return randomImage;
-				} else {
-					return null;
-				}
-			} catch (error) {
-				console.error('Error fetching image from SerpAPI:', error);
-				return null;
-			}
-		}
+    // 		if (images && images.length > 0) {
+    // 			// Pick a random image from the results
+    // 			const randomImage = images[Math.floor(Math.random() * images.length)].original;
+    // 			return randomImage;
+    // 		} else {
+    // 			return null;
+    // 		}
+    // 	} catch (error) {
+    // 		console.error('Error fetching image from SerpAPI:', error);
+    // 		return null;
+    // 	}
+    // }
 
-		// Fetch a random image of 'baked beans'
-		const imageUrl = await getRandomImage('baked beans');
+    // // Fetch a random image of 'baked beans'
+    // const imageUrl = await getRandomImage('baked beans');
 
-		// Reply with the image or an error message
-		if (imageUrl) {
-			await interaction.reply(imageUrl); // Sends the random image URL
-		} else {
-			await interaction.reply('Could not find any beans!');
-		}
-	},
+    // // Reply with the image or an error message
+    // if (imageUrl) {
+    // 	await interaction.reply(imageUrl); // Sends the random image URL
+    // } else {
+    // 	await interaction.reply('Could not find any beans!');
+    // }
+  },
 };
-
 
 // CACHE IMAGES TO REDUCE API CALLS
