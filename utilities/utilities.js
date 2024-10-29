9195f0dd392d89a27a4f0c3bf2e5391708f4f2a8
Dan James <41585360+Deehjay@users.noreply.github.com>
2024-10-29 16:22:44 +0000

    change admin to officer in check

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,6 +1,6 @@
 function hasAdminPrivileges(interaction) {
   const adminRole = interaction.guild.roles.cache.find(
-    (role) => role.name.toLowerCase() === "admin"
+    (role) => role.name.toLowerCase() === "officer"
   );
   return interaction.member.roles.cache.has(adminRole.id);
 }
